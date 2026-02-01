96bf7e4d513d6dc8e0a053778bbbdc927f8486af
lenyperis@hotmail.fr <lenyperis@hotmail.fr>
2026-02-01 19:31:40 +0100

    add(Subject test) : subject main test

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -38,4 +38,28 @@ int main(void)
 		ite++;
 		//*ite += 1;
 	}
+
+	std::cout << "--SUBJECT TEST--" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator itt = mstack.begin();
+	MutantStack<int>::iterator itte = mstack.end();
+	++itt;
+	--itt;
+	while (itt != itte)
+	{
+	std::cout << *itt << std::endl;
+	++itt;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
